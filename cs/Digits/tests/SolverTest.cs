dc319a5d019e2dfb211f1f32573b0e6853681cb6
Brian Rogers <brian-dot-net@outlook.com>
2023-08-06 09:50:09 -0700

    Solver - no solution (4)

--- cs/Digits/tests/SolverTest.cs
+++ cs/Digits/tests/SolverTest.cs
@@ -25,6 +25,14 @@ public sealed class SolverTest
         solutions.Should().BeEmpty();
     }
 
+    [Fact]
+    public void NoSolution4()
+    {
+        IList<string> solutions = Solve(1000, new[] { 1, 2, 3, 4 });
+
+        solutions.Should().BeEmpty();
+    }
+
     private static IList<string> Solve(int target, int[] numbers)
     {
         var solver = new Solver(target, new Board(numbers));
