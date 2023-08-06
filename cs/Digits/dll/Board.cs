2aa7e6be1fa1faea3777232e8ee23f6601d5546f
Brian Rogers <brian-dot-net@outlook.com>
2023-08-06 08:34:50 -0700

    Board - Move invalid (index 1 negative)

--- cs/Digits/dll/Board.cs
+++ cs/Digits/dll/Board.cs
@@ -19,7 +19,7 @@ public sealed class Board
 
     public Board Move(int n1, int n2, char op)
     {
-        if (n1 == n2 || (n1 >= _numbers.Length) || (n2 >= _numbers.Length))
+        if (n1 == n2 || (n1 < 0) || (n1 >= _numbers.Length) || (n2 >= _numbers.Length))
         {
             return Invalid;
         }
--- cs/Digits/tests/BoardTest.cs
+++ cs/Digits/tests/BoardTest.cs
@@ -33,6 +33,7 @@ public sealed class BoardTest
     [InlineData(0, 0, '+', "")]
     [InlineData(1, 10, '-', "")]
     [InlineData(6, 1, '*', "")]
+    [InlineData(-1, 2, '/', "")]
     public void MoveInvalid(int n1, int n2, char op, string result)
     {
         Board board1 = new(new[] { 1, 2, 4, 8, 16, 25 });
