21b1b464bd2d2e498944469b99a6d3c7c59886b4
Brian Rogers <brian-dot-net@outlook.com>
2023-08-06 22:25:40 -0700

    Complete Solver app

--- cs/Digits/exe/Program.cs
+++ cs/Digits/exe/Program.cs
@@ -1,15 +1,42 @@
 // Copyright (c) Brian Rogers. All rights reserved.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Digits;
 
 internal sealed class Program
 {
-    public static void Main()
-    {
-        var b = new Board(new[] { 1, 2, 3, 4, 5, 6 });
+    private static readonly Stopwatch Time = Stopwatch.StartNew();
 
-        Console.WriteLine(b);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            var parsed = Args.Parse(args);
+            var board = new Board(parsed.Numbers);
+            var solver = new Solver(parsed.Target, board);
+            Log($"Finding solutions for target={parsed.Target}, board=[{board}]...");
+            solver.Solve(ms => PrintSolution(board, ms));
+            Log("Done.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    private static void Log(string message) => Console.WriteLine("[{0:000.000}] {1}", Time.Elapsed.TotalSeconds, message);
+
+    private static void PrintSolution(Board board, IList<Move> moves)
+    {
+        foreach (Move move in moves)
+        {
+            Console.Write(board.ToString(move) + ". ");
+            board = board.TryMove(move);
+        }
+
+        Console.WriteLine();
     }
 }
