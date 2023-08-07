453b8e1e0a417b65b4ca7070d51e123e9d01d5e9
Brian Rogers <brian-dot-net@outlook.com>
2023-08-06 21:50:23 -0700

    Args - fails no args

--- cs/Digits/dll/Args.cs
+++ cs/Digits/dll/Args.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Brian Rogers. All rights reserved.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,6 +10,11 @@ public record Args(int Target, int[] Numbers)
 {
     public static Args Parse(params string[] args)
     {
+        if (args.Length < 2)
+        {
+            throw new ArgumentException("There must be at least two input values.", nameof(args));
+        }
+
         var queue = new Queue<int>(args.Select(int.Parse));
         int target = queue.Dequeue();
         int[] numbers = queue.ToArray();
--- cs/Digits/tests/ArgsTest.cs
+++ cs/Digits/tests/ArgsTest.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Brian Rogers. All rights reserved.
 
+using System;
 using FluentAssertions;
 using Xunit;
 
@@ -16,4 +17,13 @@ public sealed class ArgsTest
         args.Numbers.Should().HaveCount(1).And.ContainInOrder(2);
     }
 
+    [Fact]
+    public void ParseFailsNoArgs()
+    {
+        Action act = () => Args.Parse();
+
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("There must be at least two input values.*")
+            .Which.ParamName.Should().Be("args");
+    }
 }
