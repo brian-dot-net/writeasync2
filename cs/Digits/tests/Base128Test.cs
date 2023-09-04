d77c733f127ca5154a9bc29bfc3811544de13c3d
Brian Rogers <brian-dot-net@outlook.com>
2023-09-04 11:14:08 -0700

    Base128 - WriteFourBytes

--- cs/Digits/tests/Base128Test.cs
+++ cs/Digits/tests/Base128Test.cs
@@ -56,4 +56,23 @@ public sealed class Base128Test
         output[1].Should().Be(expected1);
         output[2].Should().Be(expected2);
     }
+
+    [Theory]
+    [InlineData(2097152, 128, 128, 128, 1)]
+    [InlineData(4194304, 128, 128, 128, 2)]
+    [InlineData(5000000, 192, 150, 177, 2)]
+    [InlineData(10000000, 128, 173, 226, 4)]
+    [InlineData(100000000, 128, 194, 215, 47)]
+    [InlineData(268435455, 255, 255, 255, 127)]
+    public void WriteFourBytes(int input, byte expected0, byte expected1, byte expected2, byte expected3)
+    {
+        Span<byte> output = stackalloc byte[4];
+
+        Base128.Write(input, output).Should().Be(4);
+
+        output[0].Should().Be(expected0);
+        output[1].Should().Be(expected1);
+        output[2].Should().Be(expected2);
+        output[3].Should().Be(expected3);
+    }
 }
