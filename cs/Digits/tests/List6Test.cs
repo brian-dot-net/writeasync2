bc56ade74741f6fd0f71f86f85d2671834f0e447
Brian Rogers <brian-dot-net@outlook.com>
2023-09-09 10:20:37 -0700

    List6 - AddFourTwoLarger

--- cs/Digits/tests/List6Test.cs
+++ cs/Digits/tests/List6Test.cs
@@ -121,4 +121,21 @@ public sealed class List6Test
         list[3].Should().Be(4);
         list[4].Should().Be(130);
     }
+
+    [Fact]
+    public void AddFourTwoLarger()
+    {
+        List6 list = default;
+
+        list = list.Add(1);
+        list = list.Add(2);
+        list = list.Add(130);
+        list = list.Add(131);
+
+        list.Count.Should().Be(4);
+        list[0].Should().Be(1);
+        list[1].Should().Be(2);
+        list[2].Should().Be(130);
+        list[3].Should().Be(131);
+    }
 }
